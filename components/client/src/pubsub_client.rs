57d45f5ad65d30f1e57b94545ceefadac6422ed8
aimeedeer <aimeez@pm.me>
2022-06-04 16:02:00 -0600

    .

--- components/client/src/pubsub_client.rs
+++ components/client/src/pubsub_client.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use crossbeam_channel::unbounded;
+use solana_client::rpc_response::SlotInfo;
 use solana_client::{
     pubsub_client::PubsubClient,
     rpc_client::RpcClient,
@@ -38,11 +40,11 @@ pub fn demo_pubsub_client(
         data_slice: None,
     });
 
-    let (account_subscription_client, account_subscription_receiver) =
+    let (mut account_subscription_client, account_subscription_receiver) =
         PubsubClient::account_subscribe(ws_url, &config.keypair.pubkey(), rpc_config.clone())?;
 
     let alice = Keypair::new();
-    let (account_subscription_client_for_alice, account_subscription_receiver_for_alice) =
+    let (mut account_subscription_client_for_alice, account_subscription_receiver_for_alice) =
         PubsubClient::account_subscribe(ws_url, &alice.pubkey(), rpc_config)?;
 
     // send a tx for testing
@@ -75,62 +77,8 @@ pub fn demo_pubsub_client(
         }
     });
 
-    println!("-------------------- slot subscription --------------------");
-    let (slot_subscription_client, slot_subscription_receiver) =
-        PubsubClient::slot_subscribe(ws_url)?;
-
-    thread::spawn(move || loop {
-        match slot_subscription_receiver.recv() {
-            Ok(result) => {
-                println!("slot subscription result: {:?}", result);
-            }
-            Err(e) => {
-                println!("slot subscription error: {:?}", e);
-                break;
-            }
-        }
-    });
-
-    println!("-------------------- root subscription --------------------");
-    let (root_subscription_client, root_subscription_receiver) =
-        PubsubClient::root_subscribe(ws_url)?;
-
-    thread::spawn(move || loop {
-        match root_subscription_receiver.recv() {
-            Ok(result) => {
-                println!("root subscription result: {:?}", result);
-            }
-            Err(e) => {
-                println!("root subscription error: {:?}", e);
-                break;
-            }
-        }
-    });
-
-    println!("-------------------- program subscription --------------------");
-    let (program_subscription_client, program_subscription_receiver) =
-        PubsubClient::program_subscribe(
-            ws_url,
-            &system_program::ID,
-            Some(RpcProgramAccountsConfig {
-                ..RpcProgramAccountsConfig::default()
-            }),
-        )?;
-
-    thread::spawn(move || loop {
-        match program_subscription_receiver.recv() {
-            Ok(response) => {
-                println!("program subscription response: {:?}", response);
-            }
-            Err(e) => {
-                println!("program subscription error: {:?}", e);
-                break;
-            }
-        }
-    });
-
     println!("-------------------- logs subscription --------------------");
-    let (logs_subscription_client, logs_subscription_receiver) = PubsubClient::logs_subscribe(
+    let (mut logs_subscription_client, logs_subscription_receiver) = PubsubClient::logs_subscribe(
         ws_url,
         RpcTransactionLogsFilter::All,
         RpcTransactionLogsConfig {
@@ -165,65 +113,43 @@ pub fn demo_pubsub_client(
         }
     });
 
-    /*
-    // error with block_subscription and vote_subscription:
-    // Error: unexpected message format: {"error": Object({"code":
-    // Number(-32601), "message": String("Method not found")}), "id":
-    // Number(1), "jsonrpc": String("2.0")}
-
-    println!("-------------------- block subscription --------------------");
-    let (block_subscription_client, block_subscription_receiver) = PubsubClient::block_subscribe(
-        ws_url,
-        RpcBlockSubscribeFilter::All,
-        Some(RpcBlockSubscribeConfig {
-            commitment: Some(CommitmentConfig {
-                commitment: CommitmentLevel::Confirmed,
+    println!("-------------------- program subscription --------------------");
+    let (mut program_subscription_client, program_subscription_receiver) =
+        PubsubClient::program_subscribe(
+            ws_url,
+            &system_program::ID,
+            Some(RpcProgramAccountsConfig {
+                ..RpcProgramAccountsConfig::default()
             }),
-            encoding: Some(UiTransactionEncoding::Json),
-            transaction_details: Some(TransactionDetails::Signatures),
-            show_rewards: None,
-            max_supported_transaction_version: None,
-        }),
-    )?;
+        )?;
 
-    thread::spawn(move || {
-        loop {
-            match block_subscription_receiver.recv() {
-                Ok(result) => {
-                    println!("block subscription result: {:?}", result);
-                }
-                Err(e) => {
-                    println!("block subscription error: {:?}", e);
-                    break;
-                }
+    thread::spawn(move || loop {
+        match program_subscription_receiver.recv() {
+            Ok(response) => {
+                println!("program subscription response: {:?}", response);
+            }
+            Err(e) => {
+                println!("program subscription error: {:?}", e);
+                break;
             }
         }
     });
 
-    block_subscription_client.send_unsubscribe();
-    block_subscription_client.shutdown();
+    println!("-------------------- root subscription --------------------");
+    let (mut root_subscription_client, root_subscription_receiver) =
+        PubsubClient::root_subscribe(ws_url)?;
 
-    println!("-------------------- vote subscription --------------------");
-    let (vote_subscription_client, vote_subscription_receiver) = PubsubClient::vote_subscribe(ws_url)?;
-
-    thread::spawn(move || {
-        loop {
-            match vote_subscription_receiver.recv() {
-                Ok(result) => {
-                    println!("vote subscription result: {:?}", result);
-                }
-                Err(e) => {
-                    println!("vote subscription error: {:?}", e);
-                    break;
-                }
+    thread::spawn(move || loop {
+        match root_subscription_receiver.recv() {
+            Ok(result) => {
+                println!("root subscription result: {:?}", result);
+            }
+            Err(e) => {
+                println!("root subscription error: {:?}", e);
+                break;
             }
         }
     });
-
-    vote_subscription_client.send_unsubscribe();
-    vote_subscription_client.shutdown();
-
-     */
 
     println!("-------------------- signature subscription --------------------");
     let alice = Keypair::new();
@@ -254,6 +180,110 @@ pub fn demo_pubsub_client(
         }
     });
 
+    println!("-------------------- slot subscription --------------------");
+    let (mut slot_subscription_client, slot_subscription_receiver) =
+        PubsubClient::slot_subscribe(ws_url)?;
+
+    thread::spawn(move || loop {
+        match slot_subscription_receiver.recv() {
+            Ok(result) => {
+                println!("slot subscription result: {:?}", result);
+            }
+            Err(e) => {
+                println!("slot subscription error: {:?}", e);
+                break;
+            }
+        }
+    });
+
+    println!("-------------------- slot updates subscription --------------------");
+    let (slot_updates_subscription_sender, slot_updates_subscription_receiver) =
+        unbounded::<SlotInfo>();
+    let handler = move |message| match slot_updates_subscription_sender.send(message) {
+        Ok(_) => (),
+        Err(e) => {
+            println!("handler receive error: {:?}", e);
+        }
+    };
+
+    let start_slot = SlotInfo {
+        slot: 95546,
+        parent: 95545,
+        root: 95514,
+    };
+    let mut slot_updates_subscription_client =
+        PubsubClient::slot_updates_subscribe(ws_url, move |_| handler(start_slot))?;
+
+    thread::spawn(move || loop {
+        match slot_updates_subscription_receiver.recv() {
+            Ok(result) => {
+                println!("slot updates subscription result: {:?}", result);
+            }
+            Err(e) => {
+                println!("slot updates subscription error: {:?}", e);
+                break;
+            }
+        }
+    });
+
+    /*
+        // error with block_subscription and vote_subscription:
+        // Error: unexpected message format: {"error": Object({"code":
+        // Number(-32601), "message": String("Method not found")}), "id":
+        // Number(1), "jsonrpc": String("2.0")}
+        println!("-------------------- block subscription --------------------");
+        let (mut block_subscription_client, block_subscription_receiver) = PubsubClient::block_subscribe(
+            ws_url,
+            RpcBlockSubscribeFilter::All,
+            Some(RpcBlockSubscribeConfig {
+                commitment: Some(CommitmentConfig {
+                    commitment: CommitmentLevel::Confirmed,
+                }),
+                encoding: Some(UiTransactionEncoding::Json),
+                transaction_details: Some(TransactionDetails::Signatures),
+                show_rewards: None,
+                max_supported_transaction_version: None,
+            }),
+        )?;
+
+        thread::spawn(move || {
+            loop {
+                match block_subscription_receiver.recv() {
+                    Ok(result) => {
+                        println!("block subscription result: {:?}", result);
+                    }
+                    Err(e) => {
+                        println!("block subscription error: {:?}", e);
+                        break;
+                    }
+                }
+            }
+        });
+
+        block_subscription_client.send_unsubscribe();
+        block_subscription_client.shutdown();
+
+        println!("-------------------- vote subscription --------------------");
+        let (mut vote_subscription_client, vote_subscription_receiver) = PubsubClient::vote_subscribe(ws_url)?;
+
+        thread::spawn(move || {
+            loop {
+                match vote_subscription_receiver.recv() {
+                    Ok(result) => {
+                        println!("vote subscription result: {:?}", result);
+                    }
+                    Err(e) => {
+                        println!("vote subscription error: {:?}", e);
+                        break;
+                    }
+                }
+            }
+        });
+
+        vote_subscription_client.send_unsubscribe();
+        vote_subscription_client.shutdown();
+    */
+
     loop {}
 
     println!("-------------------- clients unsubscribe and shutdown --------------------");
@@ -264,20 +294,22 @@ pub fn demo_pubsub_client(
     account_subscription_client_for_alice.send_unsubscribe();
     account_subscription_client_for_alice.shutdown();
 
-    slot_subscription_client.send_unsubscribe();
-    slot_subscription_client.shutdown();
-
-    root_subscription_client.send_unsubscribe();
-    root_subscription_client.shutdown();
+    logs_subscription_client.send_unsubscribe();
+    logs_subscription_client.shutdown();
 
     program_subscription_client.send_unsubscribe();
     program_subscription_client.shutdown();
 
-    logs_subscription_client.send_unsubscribe();
-    logs_subscription_client.shutdown();
+    root_subscription_client.send_unsubscribe();
+    root_subscription_client.shutdown();
 
     sig_subscription_client.send_unsubscribe();
     sig_subscription_client.shutdown();
+
+    slot_subscription_client.send_unsubscribe();
+    slot_subscription_client.shutdown();
+
+    slot_updates_subscription_client.shutdown();
 
     Ok(())
 }
